86d1635f2a71bebe90cbe5a34b56706f80fb6bb1
Miguel Oliveira <miguel_250597@hotmail.com>
2021-03-02 17:09:48 +0000

    add and use express

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 require('dotenv').config()
+const express = require('express')
 const twitchClient = require('./twitch-client')
 const utils = require('./utils')
 
-const vod = async (url, username) => {
+const app = express()
+
+app.get('/clipvodsync', async (req, res) => {
     try{
+        const { url, username } = req.query
+
         // check if the url is valid
         if(!utils.isClipUrlValid(url) && !utils.isVodUrlValid(url)) throw new Error('Invalid url')
 
@@ -11,10 +16,14 @@ const vod = async (url, username) => {
         const exactDate = await twitchClient.getExactDate(url)
         const finalVod = await twitchClient.getSyncedVod(streamerInfo, exactDate)
         
-        console.log(finalVod)
+        res.json({ streamer: streamerInfo.display_name, vod: finalVod })
     }catch(err){
-        console.log(err.message)
+        res.json({ message: err.message })
     }
-}
+})
 
-vod('https://www.twitch.tv/videos/928480589?t=3h48m30s', 'ssaab')
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Running on port ${port}`)
+})
