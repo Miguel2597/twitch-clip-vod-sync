bb9ec815ac6f8478b64a94f9d0b2763fd691cde2
Miguel Oliveira <miguel_250597@hotmail.com>
2021-03-07 17:56:26 +0000

    update chrome extension UI

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,10 +1,21 @@
+const myURL = document.getElementById('myURL')
+const myUsername = document.getElementById("myUsername")
+const myBtn = document.getElementById("myBtn")
+const mySpinner = document.getElementById("mySpinner")
+const myVod = document.getElementById('myVod')
+const myImg = document.getElementById('myImg')
+const myPar = document.getElementById('myPar')
+const myLink = document.getElementById('myLink')
+const myError = document.getElementById('myError')
+const myPar2 = document.getElementById('myPar2')
+
 document.addEventListener('DOMContentLoaded', () => {
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
         const url = tabs[0].url
-        if(isClipUrlValid(url) || isVodUrlValid(url)) document.getElementById('myURL').value = url
+        if(isClipUrlValid(url) || isVodUrlValid(url)) myURL.value = url
     })
 
-    document.getElementById("myBtn").addEventListener("click", fetchData);
+    myBtn.addEventListener("click", fetchData);
 })
 
 const isClipUrlValid = (url) => /^(?:https:\/\/)?clips\.twitch\.tv\/(\S+)$/.test(url)
@@ -12,28 +23,32 @@ const isClipUrlValid = (url) => /^(?:https:\/\/)?clips\.twitch\.tv\/(\S+)$/.test
 const isVodUrlValid = (url) => /^(?:https:\/\/)?(?:www\.)?twitch\.tv\/videos\/(\S+)\?t=\d+h\d+m\d+s$/.test(url)
 
 const fetchData = async () => {
-    document.getElementById('myVod').style.display = 'none'
-    document.getElementById("myBtn").style.display = 'none'
-    document.getElementById("mySpinner").style.display = 'block'
+    myError.style.display = 'none'
+    myVod.style.display = 'none'
+    myBtn.style.display = 'none'
+    mySpinner.style.display = 'block'
 
-    const url = document.getElementById("myURL").value
-    const username = document.getElementById("myUsername").value
+    const url = myURL.value
+    const username = myUsername.value
 
     try{
         const res = await fetch(`https://twitch-clip-vod-sync.herokuapp.com?url=${url}&username=${username}`)
         const data = await res.json()
 
         if(data.vod){
-            document.getElementById('myVod')
-            .innerHTML = `<p>${data.streamer}'s perspective</p>` + `<a href="${data.vod}" target="_blank" rel="noopener noreferrer">${data.vod}</a>`
+            // myVod.innerHTML = + `<a href="${data.vod}" target="_blank" rel="noopener noreferrer">${data.vod}</a>`
+            myImg.src = data.streamer_profile_image
+            myPar.innerText = `${data.streamer}'s perspective:`
+            myLink.href = data.vod
+            myLink.innerText = data.vod
+            myVod.style.display = 'block'
         }else{
-            document.getElementById('myVod')
-            .innerHTML =  `<p>${data.message}</p>`
+            myPar2.innerText = data.message
+            myError.style.display = 'block'
         }
         
-        document.getElementById("mySpinner").style.display = 'none'
-        document.getElementById("myBtn").style.display = 'block'
-        document.getElementById('myVod').style.display = 'block'
+        mySpinner.style.display = 'none'
+        myBtn.style.display = 'block'
 
     }catch(err){
         console.log(err)
